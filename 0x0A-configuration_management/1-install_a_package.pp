2da4b5af1740f9aac00f95a88e4ff6c04cbc1a99
joacasallas <joacasallas@gmail.com>
2022-04-19 16:45:01 -0500

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
 # Using Puppet, install flask from pip3.
-package {'flask':
-  ensure => 'installed',
-  install_options => ['-v', '2.1.0'],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
 }
+
+# package {'flask': ensure => installed, install_options => ['-v', '2.1.0']}
