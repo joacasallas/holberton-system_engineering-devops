b9b786ebc02a844300f729bf7aba5c0d8b93f9e1
joacasallas <joacasallas@gmail.com>
2022-04-28 17:05:55 -0500

    update

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,15 @@
+#install nginx with puppet
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'nginx':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+  content => 'Hello World!',
+}
